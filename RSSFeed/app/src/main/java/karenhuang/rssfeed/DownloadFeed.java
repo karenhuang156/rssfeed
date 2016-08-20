5de5ef707f0656eb9f85219a12e336e318b6ce35
karenhuang <karenhuang156@gmail.com>
2016-08-20 13:13:15 +1000

    commit - need to fix execute.URL in MainActivity

--- RSSFeed/app/src/main/java/karenhuang/rssfeed/DownloadFeed.java
+++ RSSFeed/app/src/main/java/karenhuang/rssfeed/DownloadFeed.java
@@ -1,8 +1,83 @@
 package karenhuang.rssfeed;
 
+import android.os.AsyncTask;
+import android.webkit.WebView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+
 /**
  * Created by karenhuang on 20/08/16.
  */
-public class DownloadFeed {
+public class DownloadFeed extends AsyncTask<String, Void, String>{
+    @Override
+    protected String doInBackground(String... urls) {
+       try {
+           return loadXml(urls[0]);
+       } catch (IOException e){
+           return null;
+       }   catch (XmlPullParserException e) {
+           return null;
+       }
 
-}
+    }
+    @Override
+    protected void onPostExecute(String result) {
+        //setContentView(R.layout.content_main){
+            //displays html string in UI via a WebView
+           // WebView myWebView = (WebView) findViewById(R.id.webview);
+            //myWebView.loadData(result, "text/html", null);
+        }
+
+    private String loadXml(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        //Instantiate parser
+        SmhXmlParser smhParser = new SmhXmlParser();
+        List<Item> items = null;
+
+        StringBuilder displayString = new StringBuilder();
+        try {
+            stream = downloadUrl(urlString);
+            items = smhParser.parse(stream);
+
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        for (Item item : items) {
+            //append the link
+            displayString.append("<p><a href='");
+            displayString.append(item.link);
+            displayString.append("'>" + item.title + "</a></p>");
+            displayString.append("<p>" + item.description + "</p>");
+            displayString.append("<p>" + item.creator+ "</p>");
+            displayString.append("<p>" + item.date + "</p>");
+        }
+        return displayString.toString();
+        }
+
+    // Given a string representation of a URL, sets up a connection and gets
+    // an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+
+        //Add in getResponseCode() for logging - refer to slide 32
+    }
+
+    }
--- RSSFeed/app/src/main/java/karenhuang/rssfeed/MainActivity.java
+++ RSSFeed/app/src/main/java/karenhuang/rssfeed/MainActivity.java
@@ -9,8 +9,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final String smhURL = "http://www.smh.com.au/rssheadlines/top.xml";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        setContentView(R.layout.activity_main);
+        loadSmhFeed();
     }
 
     @Override
@@ -51,6 +48,6 @@ public class MainActivity extends AppCompatActivity {
     }
     //Use Asyntask to download the XML Feed from SMH.
     public void loadSmhFeed(){
-        new DownloadXmlTask.execute(URL);
+        DownloadFeed downloadFeed = new DownloadFeed.execute(smhURL);
     }
 }
